e39a62244bb4519c5316b1ba8a4491f5ff0a118a
xtof <cerisara@loria.fr>
2022-03-15 09:11:10 +0100

    doc report

--- xtof/sync.sh
+++ xtof/sync.sh
@@ -1,2 +1,6 @@
-rsync -av lully:github/weibull-knowledge-informed-ml/xtof/lightning_logs ./lightning_logs
+rsync -av lully:github/weibull-knowledge-informed-ml/xtof/lightning_logs ./
 tensorboard --logdir ./lightning_logs/
+
+exit
+
+convert -density 300 -background black /home/xtof/Downloads/equiloss.svg eq.jpg
